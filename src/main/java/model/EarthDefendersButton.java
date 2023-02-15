2af59dcb9f52c0f20a2f719ff4d1dd36cf33357b
bryan <bigavilanezb00@elpuig.xeill.net>
2023-02-15 17:23:56 +0100

    BotonesCreados

--- src/main/java/model/EarthDefendersButton.java
+++ src/main/java/model/EarthDefendersButton.java
@@ -1,6 +1,10 @@
 package model;
 
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Font;
 
 import java.io.FileInputStream;
@@ -8,21 +12,74 @@ import java.io.FileNotFoundException;
 
 public class EarthDefendersButton extends Button {
 
-    private final String FONT_PATH = "/model/resources/kenvector_future.ttf";
-    private final String BUTTON_PRESSED_STYLE = "-fx-background-color: transparent; -fx-background-image: url('/model/resource/yellow_button_pressed');";
-    private final String BUTTON_STYLE = "-fx-background-color: transparent; -fx-background-image: url('/model/resource/yellow_button');";
+    private final String FONT_PATH = "src/main/java/model/resources/kenvector_future.ttf";
+    private final String BUTTON_PRESSED_STYLE = "-fx-background-color: transparent;" +
+            "-fx-background-image: url('file:src/main/java/model/resources/blue_button_pressed.png');";
+    private final String BUTTON_STYLE = "-fx-background-color: transparent;" +
+            "-fx-background-image: url('file:src/main/java/model/resources/blue_button_free.png');";
 
     public EarthDefendersButton(String text) {
         setText(text);
+        setButtonFont();
+        setPrefWidth(190);
+        setPrefHeight(49);
+        setStyle(BUTTON_STYLE);
+        initializeButtonListeners();
     }
 
     private void setButtonFont() {
 
         try {
             setFont(Font.loadFont(new FileInputStream(FONT_PATH), 23));
-        } catch ( FileNotFoundException e) {
-            setFont(Font.font("Verdana",23));
+        } catch (FileNotFoundException e) {
+            setFont(Font.font("Verdana", 23));
         }
     }
 
+    private void setButtonPressedStyle() {
+        setStyle(BUTTON_PRESSED_STYLE);
+        setPrefHeight(45);
+        setLayoutY(getLayoutY()+ 4);
+    }
+
+    private void setButtonReleasedStyle() {
+        setStyle(BUTTON_STYLE);
+        setPrefHeight(49);
+        setLayoutY(getLayoutY() - 4);
+    }
+
+    private void initializeButtonListeners() {
+
+        setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if (event.getButton().equals(MouseButton.PRIMARY)) {
+                    setButtonPressedStyle();
+                }
+            }
+        });
+
+        setOnMouseReleased(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                if (event.getButton().equals(MouseButton.PRIMARY)) {
+                    setButtonReleasedStyle();
+                }
+            }
+        });
+
+        setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                setEffect(new DropShadow());
+            }
+        });
+
+        setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                setEffect(null);
+            }
+        });
+    }
 }
--- src/main/java/view/ViewManager.java
+++ src/main/java/view/ViewManager.java
@@ -1,34 +1,84 @@
 package view;
 
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import model.EarthDefendersButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewManager {
 
-    private static final int HEIGHT = 600;
-    private static final int WIDTH = 800;
+    private static final int HEIGHT = 768;
+    private static final int WIDTH = 1024;
     private AnchorPane mainPane;
     private Scene mainScene;
     private Stage mainStage;
 
+    private final static int MENU_BUTTONS_START_X = 100;
+    private final static int MENU_BUTTONS_START_Y = 150;
+    List<EarthDefendersButton> menuButtons;
+
     public ViewManager () {
+        menuButtons = new ArrayList<>();
         mainPane = new AnchorPane();
         mainScene = new Scene(mainPane,WIDTH, HEIGHT);
         mainStage = new Stage();
         mainStage.setScene(mainScene);
         createButtons();
+        createBackground();
     }
 
     public Stage getMainStage() {
         return mainStage;
     }
 
-    private void createButtons() {
-
+    private void addMenuButton(EarthDefendersButton button) {
+        button.setLayoutX(MENU_BUTTONS_START_X);
+        button.setLayoutY(MENU_BUTTONS_START_Y + menuButtons.size()* 100);
+        menuButtons.add(button);
+        mainPane.getChildren().add(button);
     }
 
+    private void createButtons() {
+        createStartButton();
+        createScoreButton();
+        createHelpButton();
+        createCreditsButton();
+        createExitButton();
+    }
+
+    private void createStartButton() {
+        EarthDefendersButton startButton = new EarthDefendersButton("JUGAR");
+        addMenuButton(startButton);
+    }
+
+    private void createScoreButton() {
+        EarthDefendersButton scoreButton = new EarthDefendersButton("SCORES");
+        addMenuButton(scoreButton);
+    }
+
+    private void createHelpButton() {
+        EarthDefendersButton helpButton = new EarthDefendersButton("AYUDA");
+        addMenuButton(helpButton);
+    }
+
+    private void createCreditsButton() {
+        EarthDefendersButton creditsButton = new EarthDefendersButton("CREDITOS");
+        addMenuButton(creditsButton);
+    }
+
+    private void createExitButton() {
+        EarthDefendersButton exitButton = new EarthDefendersButton("SALIR");
+        addMenuButton(exitButton);
+    }
+
+    private void createBackground() {
+        Image backgroundImage = new Image("file:src/main/java/view/resources/darkPurple.png", 256, 256, false, true);
+        BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.REPEAT,
+                BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, null);
+        mainPane.setBackground(new Background(background));
+    }
 }
